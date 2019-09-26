cb8e69f4a204cca291691a19e97d5df0421b335f
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-26 21:37:52 +0700

    Changing the way of importing module "plots"
    Relevant functions from separate sub-modules (e.g. four_plot, histogram, etc.) are now imported to the main module "plots" before entering the code environment.

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,1 +1,5 @@
-
+from plots.four_plot import four_plot
+from plots.histogram import histogram
+from plots.lag_plot import lag_plot
+from plots.probability_plot import probability_plot
+from plots.run_sequence_plot import run_sequence_plot
