d396e46065c598199d234ced7784e798ed005d1e
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-27 22:16:52 +0700

    Adding the normal distribution pdf
    A function calculating the probability density function of the normal distribution has been defined. This function can be used to overlay a pdf plot on a histogram.

--- utils/calculations.py
+++ utils/calculations.py
@@ -12,9 +12,25 @@ def box_cox_transformation(x, lambda_):
     The Box-Cox transformation is defined as: T(X) = (X^λ - 1) / λ,
     where X is the response variable and λ is the transformation
     parameter. For λ = 0, the log of the data is taken instead of using
-    the above formula. 
+    the above formula.
     """
     if lambda_ == 0:
         return np.log(x) / np.log(10)
     else:
         return (x**lambda_ - 1) / lambda_
+
+
+def normal_pdf(x, mean=0, std=1):
+    """
+    The probability density function of a normal distribution with a
+    mean of mu and standard deviation of sigma is given by the following
+    formula:
+    f(x) = (1/sqrt(2*pi*sigma^2)) * e^(-(x-mu)^2 / (2*sigma^2))
+
+    This function (normal_pdf) can take a number or a numpy array for
+    argument x.
+    """
+    coef = 1/np.sqrt(2*np.pi*(std**2))
+    power = -(x-mean)**2 / (2*(std**2))
+    return coef * np.exp(power)
+    
