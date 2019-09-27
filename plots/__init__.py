07c87bea6bbff283eccea436d2f4801617ca90a9
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-28 06:50:52 +0700

    Adding import statements for sub-modules box_cox_linearity_plot and scatter_plot

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,3 +1,5 @@
+from box_cox_linearity_plot import (box_cox_linearity_plot,
+                                    box_cox_linearity_set)
 from box_cox_normality_plot import (box_cox_normality_plot,
                                     box_cox_normality_set)
 from plots.four_plot import four_plot
@@ -6,3 +8,4 @@ from plots.lag_plot import lag_plot
 from plots.ppcc_plot import ppcc_plot
 from plots.probability_plot import probability_plot
 from plots.run_sequence_plot import run_sequence_plot
+from scatter_plot import scatter_plot
