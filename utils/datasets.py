b8e1ef26772d8810ae6bd204ea59650cf499de3c
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-27 18:43:56 +0700

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -91,10 +91,31 @@ def load_normal():
                         url_to_fetch_if_missing=url)
 
 
+def load_water_density():
+    """Source: NIST Engineering Handbook of Statistical Methods"""
+    url = "https://www.itl.nist.gov/div898/handbook/datasets/JONES.DAT"
+    filename = url.split("/")[-1]
+
+    return load_dataset(filename, names=["Density", "Temperature"],
+                        skiprows=25, url_to_fetch_if_missing=url,
+                        delim_whitespace=True)
+
+
+def load_alaska_pipeline():
+    """Source: NIST Engineering Handbook of Statistical Methods"""
+    url = "https://www.itl.nist.gov/div898/handbook/datasets/BERGER1.DAT"
+    filename = url.split("/")[-1]
+    names = ["In-Field Defect Size", "In-Lab Defect Size", "Batch"]
+    
+    return load_dataset(filename, names=names, skiprows=25,
+                        url_to_fetch_if_missing=url,
+                        delim_whitespace=True)
+
+
     
 # TESTING
 if __name__ == "__main__":
-    df = load_normal()
+    df = load_alaska_pipeline()
     print(df.info())
     print(df.head())
     print(df.tail())
