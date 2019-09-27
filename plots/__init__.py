52f85604f2e0b14d00e2cc0396a000ca4d524dd9
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-27 07:51:25 +0700

    Update __init__.py

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,5 +1,6 @@
 from plots.four_plot import four_plot
 from plots.histogram import histogram
 from plots.lag_plot import lag_plot
+from plots.ppcc_plot import ppcc_plot
 from plots.probability_plot import probability_plot
 from plots.run_sequence_plot import run_sequence_plot
