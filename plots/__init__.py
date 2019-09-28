c18ef0a2c80b82fcb13dd69040c2c7bee0595510
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-28 20:44:50 +0700

    Changing some import statements to ensure consistency

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,13 +1,13 @@
-from bootstrap_plot import bootstrap_plot
-from box_cox_linearity_plot import (box_cox_linearity_plot,
-                                    box_cox_linearity_set)
-from box_cox_normality_plot import (box_cox_normality_plot,
-                                    box_cox_normality_set)
+from plots.bootstrap_plot import bootstrap_plot
+from plots.box_cox_linearity_plot import (box_cox_linearity_plot,
+                                          box_cox_linearity_set)
+from plots.box_cox_normality_plot import (box_cox_normality_plot,
+                                          box_cox_normality_set)
 from plots.four_plot import four_plot
 from plots.histogram import histogram
 from plots.lag_plot import lag_plot
 from plots.ppcc_plot import ppcc_plot
 from plots.probability_plot import probability_plot
 from plots.run_sequence_plot import run_sequence_plot
-from scatter_plot import scatter_plot
-from weibull_plot import weibull_plot
+from plots.scatter_plot import scatter_plot
+from plots.weibull_plot import weibull_plot
