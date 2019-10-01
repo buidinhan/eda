84fcc4b05099df057fa9fe881b06b355085fce54
An Bui <54894303+buidinhan@users.noreply.github.com>
2019-10-02 06:23:41 +0700

    Minor changes

--- plots/run_sequence_plot.py
+++ plots/run_sequence_plot.py
@@ -1,10 +1,12 @@
 """
-Source
-------
+RUN SEQUENCE PLOT
+
+## Source
 https://www.itl.nist.gov/div898/handbook/eda/section3/runseqpl.htm
 
-Purpose: Check for Shifts in Location and Scale and Outliers
-------------------------------------------------------------
+## Purpose
+Check for Shifts in Location and Scale and Outliers
+
 Run sequence plots (Chambers 1983) are an easy way to graphically
 summarize a univariate data set. A common assumption of univariate
 data sets is that they behave like:
@@ -16,14 +18,14 @@ data sets is that they behave like:
 With run sequence plots, shifts in location and scale are typically
 quite evident. Also, outliers can easily be detected.
 
-Definition: y(i) Versus i
--------------------------
+## Definition
+y(i) Versus i
+
 Run sequence plots are formed by:
 * Vertical axis: Response variable Y_i
 * Horizontal axis: Index i (i = 1, 2, 3, ... )
 
-Questions
----------
+## Questions
 The run sequence plot can be used to answer the following questions
 1. Are there any shifts in location?
 2. Are there any shifts in variation?
@@ -32,8 +34,9 @@ The run sequence plot can be used to answer the following questions
 The run sequence plot can also give the analyst an excellent feel for
 the data.
 
-Importance: Check Univariate Assumptions
-----------------------------------------
+## Importance
+Check Univariate Assumptions
+
 For univariate data, the default model is: Y = constant + error, where
 the error is assumed to be random, from a fixed distribution, and with
 constant location and scale. The validity of this model depends on the
@@ -44,13 +47,6 @@ Even for more complex models, the assumptions on the error term are
 still often the same. That is, a run sequence plot of the residuals
 (even from very complex models) is still vital for checking for
 outliers and for detecting shifts in location and scale.
-
-Related Techniques
-------------------
-* Scatter Plot
-* Histogram
-* Autocorrelation Plot
-* Lag Plot
 """
 
 
