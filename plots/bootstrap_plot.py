dab8ad614bdcbd46283c7316eddeb55b19d074ba
An Bui <54894303+buidinhan@users.noreply.github.com>
2019-10-02 05:59:19 +0700

    Minor changes

--- plots/bootstrap_plot.py
+++ plots/bootstrap_plot.py
@@ -1,17 +1,16 @@
 """
-Source
-------
+BOOTSTRAP PLOT
+
+## Source
 https://www.itl.nist.gov/div898/handbook/eda/section3/bootplot.htm
 
-Purpose
--------
+## Purpose
 Estimate uncertainty
 
 The bootstrap (Efron and Gong) plot is used to estimate the uncertainty
 of a statistic.
 
-Generate subsamples with replacement
-------------------------------------
+## Generate subsamples with replacement
 To generate a bootstrap uncertainty estimate for a given statistic from
 a set of data, a subsample of a size less than or equal to the size of
 the data set is generated from the data, and the statistic is calculated.
@@ -32,17 +31,7 @@ are sorted into ascending order and the value of the 25th median
 limit while the value of the 475th median (assuming exactly 500
 subsamples were taken) is the upper confidence limit.
 
-Sample Plot
------------
-This bootstrap plot was generated from 500 uniform random numbers.
-Bootstrap plots and corresponding histograms were generated for the
-mean, median, and mid-range. The histograms for the corresponding
-statistics clearly show that for uniform random numbers the mid-range
-has the smallest variance and is, therefore, a superior location
-estimator to the mean or the median.
-
-Definition
-----------
+## Definition
 The bootstrap plot is formed by:
 * Vertical axis: Computed value of the desired statistic for a given
 subsample.
@@ -54,8 +43,7 @@ for the desired statistic. This is usually immediately followed by a
 histogram or some other distributional plot to show the location and
 variation of the sampling distribution of the statistic.
 
-Questions
----------
+## Questions
 The bootstrap plot is used to answer the following questions:
 1. What does the sampling distribution for the statistic look like?
 2. What is a 95% confidence interval for the statistic?
@@ -63,26 +51,19 @@ The bootstrap plot is used to answer the following questions:
 variance? That is, which statistic generates the narrowest confidence
 interval? 
 
-Importance
-----------
+## Importance
 The most common uncertainty calculation is generating a confidence
 interval for the mean. In this case, the uncertainty formula can be
 derived mathematically. However, there are many situations in which the
 uncertainty formulas are mathematically intractable. The bootstrap
 provides a method for calculating the uncertainty in these cases.
 
-Caution on use of the bootstrap
--------------------------------
+## Caution on use of the bootstrap
 The bootstrap is not appropriate for all distributions and statistics
 (Efron and Tibrashani). For example, because of the shape of the uniform
 distribution, the bootstrap is not appropriate for estimating the
 distribution of statistics that are heavily dependent on the tails,
 such as the range.
-
-Related Techniques
-------------------
-* Histogram
-* Jackknife
 """
 
 
